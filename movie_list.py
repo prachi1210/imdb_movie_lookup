9ff87648981faa906055d758ade0395d9e9516c3
Prachi <prachi121096@gmail.com>
2016-07-06 21:59:43 +0530

    chore:get imdb id from page search + use api to get info

--- movie_list.py
+++ movie_list.py
@@ -2,17 +2,33 @@ import urllib
 from urllib2 import Request, urlopen, URLError
 import json
 import pandas as pd
+import glob, os
+import requests
+from lxml import html
 
 genre = []
 plot = []
 ratings = []
-def get_ratings(title):
-    omdb_request = Request('http://www.omdbapi.com/?t='+urllib.quote_plus(title)+'&y=&plot=short&r=json')
+
+def get_imdb_id(input):
+    """Function to get imdb id from input file name"""
+    query = urllib.quote_plus(input)
+    url = "http://www.imdb.com/find?ref_=nv_sr_fn&q="+query+"&s=all"
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    imdb_id=(tree.xpath('//td[@class="result_text"]//a')[0].get('href'))
+    imdb_id = imdb_id.replace('/title/','')
+    imdb_id = imdb_id.replace('/?ref_=fn_al_tt_1','')
+    return (imdb_id)
+
+def get_info(id):  
+    """Function to get genre, plot and ratings from imdb id"""
+    omdb_request = Request('http://www.omdbapi.com/?i='+id+'&y=&plot=short&r=json')
     response = urlopen(omdb_request)
     data = response.read()
     d=json.loads(data)
     if 'False' in data:
-        message = "No such movie"
+        message = "No results found"
         genre.append(message)
         plot.append(message)
         ratings.append(message)
@@ -21,10 +37,10 @@ def get_ratings(title):
         genre.append(d['Genre'])
         plot.append(d['Plot'])
         ratings.append(d['imdbRating'])
-    
-get_ratings('toy story')
-get_ratings('titanic')
-get_ratings('prachi')
+
+   
+get_info(get_imdb_id('toy story'))
+
 
 df = pd.DataFrame({'Genre': genre, 'Plot': plot, 'Ratings': ratings})
 
